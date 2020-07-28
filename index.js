06f717b82d96cb6a7d7666e3e070dec830758681
EricaSugui <ericasugui@gmail.com>
2020-07-28 17:53:02 -0300

    adding POST method

--- index.js
+++ index.js
@@ -4,8 +4,29 @@ const server = express();
 server.use(express.json());
 
 
+const users = ['kelly', 'dani', 'maycon', 'allan'];
+
+function checkUsersInArray(req, res, next) {
+  const user = users[req.params.index];
+  if (!user) {
+    return res.status(400).json({ error: `Index doesn't exists`});
+  }
+  req.user = user;
+  return next();
+}
+
 server.get('/users/', (req, res) => {
-  return res.send('A rota esta up');
+  //return res.send('A rota esta up');
+  return res.json(users);
+});
+server.get('/users/:index', checkUsersInArray, (req, res) => {
+  return res.json(req.user);
+});
+
+server.post('/users/', (req, res) => {
+  const { name } = req.body;
+  users.push(name);
+  return res.json(users);
 });
 
 
